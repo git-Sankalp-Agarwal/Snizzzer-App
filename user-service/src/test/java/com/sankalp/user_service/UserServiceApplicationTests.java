6877607ac7ffe0f3648e4b9089f4d9add94cf455
Sankalp Agarwal <sankalpagarwal1304@gmail.com>
2025-02-18 01:36:58 +0530

    initial commit

---
+++ user-service/src/test/java/com/sankalp/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sankalp.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
